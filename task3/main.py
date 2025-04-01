8bed155b61741e6b8c43d0f2cf2edf718f16dc7c
Reshetnikova Natalia <nataliareshetnikova943@gmail.com>
2025-04-01 17:44:20 +0700

    add chebyshev points

--- task3/main.py
+++ task3/main.py
@@ -1,27 +1,29 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
+import time
 N_MAX = 11
 
 
-def solution(n):
-    xs = np.array([2 * i / n - 1 for i in range(n + 1)])
+def solution(n, chebyshev=False):
+    if chebyshev:
+        a, b = (-1, 1)
+        xs = np.array([(b + a + (b - a) * np.cos((2 * i + 1) * np.pi / (2 * (n + 1)))) / 2 for i in range(n + 1)])
+    else:
+        xs = np.array([2 * i / n - 1 for i in range(n + 1)])
     f = 1 / (1 + 25 * xs ** 2)
     y = np.zeros((n + 1, n + 1))
-    for i in range(n + 1):
-        y[i][i] = f[i]
+    y[:,0] = f
 
-    for i in range(n + 1):
-        for j in range(i + 1, n + 1):
-            y[i][j] = (y[i + 1][j] - y[i][j - 1]) / (xs[j] - xs[i])
+    for j in range(1, n + 1):
+        for i in range(n - j + 1):
+            y[i, j] = (y[i + 1, j - 1] - y[i, j - 1]) / (xs[i + j] - xs[i])
 
     def polynomial(x):
         result = y[0][0]
+        product = 1
         for i in range(n):
-            temp = 0
-            for j in range(i + 1, n + 1):
-                temp += y[0][j]
-            result += temp * (x - xs[i])
+            product *= x - xs[i]
+            result += y[0][i + 1] * product
         return result
 
     def function(x):
@@ -30,12 +32,17 @@ def solution(n):
     x_values = np.array(range(-1000, 1000)) * 0.001
     polynomial_values = np.vectorize(polynomial)(x_values)
     f_values = np.vectorize(function)(x_values)
-    y_value = np.vectorize(abs)(polynomial_values - f_values)
-    plt.scatter(x_values, y_value, c=str(1 - n / N_MAX), linewidths=0.1)
+    y_values = np.vectorize(abs)(polynomial_values - f_values)
+    plt.scatter(x_values, y_values, c=str(1 - n / N_MAX), linewidths=0.1)
     # чем темнее, тем больше n
 
 
-n = 3
 for n in range(3, N_MAX):
     solution(n)
+plt.show()
+
+plt.cla()
+
+for n in range(3, N_MAX):
+    solution(n, True)
 plt.show()
