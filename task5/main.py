0734d4fc85c4eceb25d64e6564e059b13295a9b7
Natalia Reshetnikova <nataliareshetnikova943@gmail.com>
2025-04-17 13:10:33 +0700

    find approximation

--- task5/main.py
+++ task5/main.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def solve_poisson(y0_type=0, y0=0, yn_type=1, yn=2, n=100):
+def function(y0_type=0, y0=0, yn_type=0, yn=0, n=100):
     if y0_type == yn_type == 1:
         raise ValueError("Граничные условия не должны быть только производными")
     a = -np.pi / 2
@@ -22,6 +22,7 @@ def solve_poisson(y0_type=0, y0=0, yn_type=1, yn=2, n=100):
 
     if y0_type == 0:
         D[1] -= y0
+
         D[0] = y0
     elif y0_type == 1:
         B[1] = -1
@@ -53,9 +54,32 @@ def solve_poisson(y0_type=0, y0=0, yn_type=1, yn=2, n=100):
     if yn_type == 1:
         D[-1] = dn + 2 * D[-3] - D[-2]
 
-    plt.plot(x, D)
-    plt.grid()
-    plt.show()
+    return x, D
 
 
-solve_poisson()
+def f(x, c1, c2):
+    return -np.cos(x) + c1 * x + c2 * np.ones(len(x))
+
+
+def find_max_diff(f, D):
+    return max(abs(f - D))
+
+
+def find_approximation(n=100):
+    x1, D1 = function(0, 0, 0, 0, n=n)
+    f1 = f(x1, 0, 0)
+    epsilon1 = find_max_diff(f1, D1)
+
+    x2, D2 = function(0, 0, 0, 0, n=n * 2)
+    f2 = f(x2, 0, 0)
+    epsilon2 = find_max_diff(f2, D2)
+
+    return np.log2(epsilon1 / epsilon2)
+
+
+print("Порядок аппроксимации:", find_approximation())
+
+x, D = function()
+plt.plot(x, D)
+plt.grid()
+plt.show()
